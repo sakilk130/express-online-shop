13d79cc9f8183458c09f3ffebde0d97cb366b370
sakilk130 <sakilk130@gmail.com>
2021-04-19 22:16:29 +0600

    delete from product list also cart in json file

--- models/product.js
+++ models/product.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const Cart = require('./cart');
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
@@ -50,9 +51,12 @@ module.exports = class Product {
 
   static deleteById(id) {
     getProductsFromFile((products) => {
+      const product = products.find((prod) => prod.id === id);
       const updatedProduct = products.filter((p) => p.id !== id);
       fs.writeFile(p, JSON.stringify(updatedProduct), (err) => {
-        console.log(err);
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
+        }
       });
     });
   }
